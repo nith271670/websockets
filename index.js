f2bbf01d7e7da63a60b28c4d7c047e16e3a84422
nith271670 <nimisha.thilak@elektrobit.com>
2019-01-02 15:33:50 +0530

    Changed the server conf

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// var WebSocketServer = require("ws").Server
+// var http = require("http")
+// var express = require("express")
+// var app = express()
+// var port = process.env.PORT || 5000
+
+// app.use(express.static(__dirname + "/"))
+
+// var server = http.createServer(app)
+// server.listen(port)
+
 var server = require("ws").Server;
-var s = new server({port:5001});
+var s = new server({port: process.env.PORT || 5000});
 
 s.on("connection", function(ws){
     ws.on("message",function(message){
