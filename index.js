6c81777bcc288aedbd71679810785e0fd079ea67
nith271670 <nimisha.thilak@elektrobit.com>
2019-01-02 16:07:40 +0530

    include npm start

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var server = require("ws").Server
+var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
 var app = express()
@@ -6,33 +6,23 @@ var port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + "/"))
 
-var s = http.createServer(app)
-s.listen(port)
-// /**********/
+var server = http.createServer(app)
+server.listen(port)
 
-// var server = require("ws").Server;
-// var s = new server({port: process.env.PORT || 5000});
+console.log("http server listening on %d", port)
 
-s.on("connection", function(ws){
-    ws.on("message",function(message){
-        message = JSON.parse(message);
-        if(message.type == 'Name'){
-            ws.personName = message.data;
-            return;
-        }
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
 
-        console.log("Received"+message);
-        s.clients.forEach(function e(client){
-            if(client != ws)
-           {
-                client.send(JSON.stringify({
-                name: ws.personName,
-                 data: message.data
-             }));
-           }
-                
-        })
-            // ws.send("from server: "+message);
-        
-    })
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
 })
